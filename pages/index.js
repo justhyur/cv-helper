a4ec46f2756be874e159c1e6f3e61968b46b192b
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-10-30 14:25:17 -0100

    test new index

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js test!</a>
+          Welcome to <a href="https://nextjs.org">Next.js test 223!</a>
         </h1>
 
         <p className={styles.description}>
